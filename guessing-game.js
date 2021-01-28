f9d45ab91e5058b8e9763a1ef9c468e876efdf05
Laura <lkzailac@gmail.com>
2021-01-28 18:09:17 -0500

    pseudocode

---
+++ guessing-game.js
@@ -0,0 +1,10 @@
+/*
+Guess Secret Number between num1 and num2
+let secretNum = random num ??
+-user makes guess
+-if guess = secretNum, return "correct"
+-if user input too high, return "Too High"
+   and recall the question
+-else return "Too Low"
+    and recall the question
+*/
